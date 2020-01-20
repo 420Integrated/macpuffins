a8ae9f4564cf4947c59ae80307b919816532faa9
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-01-20 12:28:27 -0400

    macpuffins

--- ci_scripts/test_linux-gui_wallet.py
+++ ci_scripts/test_linux-gui_wallet.py
@@ -4,7 +4,7 @@
 import os
 import argparse
 import multiprocessing as mp
-import neblio_ci_libs as nci
+import macpuffins_ci_libs as nci
 
 nci.setup_travis_or_gh_actions_env_vars()
 
@@ -70,17 +70,17 @@ os.environ['QRENCODE_LIB_PATH'] = openssl_lib_path
 os.environ['PATH'] = '/usr/lib/ccache:' + os.environ['PATH']
 
 if (args.test):
-    nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=1" "RELEASE=1" "OPENSSL_INCLUDE_PATH=' + openssl_include_path + '" "OPENSSL_LIB_PATH=' + openssl_lib_path + '" "QRENCODE_LIB_PATH=' + qrencode_lib_path + '" "QRENCODE_INCLUDE_PATH=' + qrencode_include_path + '" "PKG_CONFIG_PATH=' + pkg_config_path + '" "NEBLIO_CONFIG += NoWallet" ../neblio-wallet.pro')
+    nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=1" "RELEASE=1" "OPENSSL_INCLUDE_PATH=' + openssl_include_path + '" "OPENSSL_LIB_PATH=' + openssl_lib_path + '" "QRENCODE_LIB_PATH=' + qrencode_lib_path + '" "QRENCODE_INCLUDE_PATH=' + qrencode_include_path + '" "PKG_CONFIG_PATH=' + pkg_config_path + '" "MACPUFFINS_CONFIG += NoWallet" ../macpuffins-wallet.pro')
     nci.call_with_err_code("make -j" + str(mp.cpu_count()))
     # run tests
-    nci.call_with_err_code("./wallet/test/neblio-tests")
+    nci.call_with_err_code("./wallet/test/macpuffins-tests")
 else:
-    nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=1" "RELEASE=1" "OPENSSL_INCLUDE_PATH=' + openssl_include_path + '" "OPENSSL_LIB_PATH=' + openssl_lib_path + '" "QRENCODE_LIB_PATH=' + qrencode_lib_path + '" "QRENCODE_INCLUDE_PATH=' + qrencode_include_path + '" "PKG_CONFIG_PATH=' + pkg_config_path + '" ../neblio-wallet.pro')
+    nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=1" "RELEASE=1" "OPENSSL_INCLUDE_PATH=' + openssl_include_path + '" "OPENSSL_LIB_PATH=' + openssl_lib_path + '" "QRENCODE_LIB_PATH=' + qrencode_lib_path + '" "QRENCODE_INCLUDE_PATH=' + qrencode_include_path + '" "PKG_CONFIG_PATH=' + pkg_config_path + '" ../macpuffins-wallet.pro')
     nci.call_with_err_code("make -j" + str(mp.cpu_count()))
 
-    file_name = '$(date +%Y-%m-%d)---' + os.environ['BRANCH'] + '-' + os.environ['COMMIT'][:7] + '---neblio-Qt---ubuntu16.04.tar.gz'
+    file_name = '$(date +%Y-%m-%d)---' + os.environ['BRANCH'] + '-' + os.environ['COMMIT'][:7] + '---macpuffins-Qt---ubuntu16.04.tar.gz'
 
-    nci.call_with_err_code('tar -zcvf "' + file_name + '" -C ./wallet neblio-qt')
+    nci.call_with_err_code('tar -zcvf "' + file_name + '" -C ./wallet macpuffins-qt')
     nci.call_with_err_code('mv ' + file_name + ' ' + deploy_dir)
     nci.call_with_err_code('echo "Binary package at ' + deploy_dir + file_name + '"')
     # set the SOURCE_DIR & SOURCE_PATH env vars, these point to the binary that will be uploaded
