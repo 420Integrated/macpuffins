109bf8e151687c0cf2b6690c32e3a105aa6c382e
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-01-20 12:23:19 -0400

    macpuffins

--- ci_scripts/release_post_processing.py
+++ ci_scripts/release_post_processing.py
@@ -6,7 +6,7 @@ import time
 
 gh = github3.login(token=os.environ['GITHUB_TOKEN'])
 
-repo = gh.repository('NeblioTeam', 'neblio')
+repo = gh.repository('macpuffin', 'macpuffins')
 
 win_q_str = ''
 mac_q_str = ''
@@ -22,12 +22,12 @@ docker_rpi_d_str = ''
 def check_assets():
     print("Checking Release for Assets")
     body = []
-    win_icon        = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/windows-10-100.png" alt="Windows Icon by icons8 from https://icons8.com/" height="48px"/>'
-    mac_icon        = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/apple-100.png" alt="Apple Icon by icons8 from https://icons8.com/" height="48px"/>'
-    lin_icon        = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/linux-100.png" alt="Linux Icon by icons8 from https://icons8.com/" height="48px"/>'
-    rpi_icon        = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/raspberry-pi-100.png" alt="Raspberry Pi Icon by icons8 from https://icons8.com/" height="48px"/>'
-    docker_icon     = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/docker-100.png" alt="Docker Icon by icons8 from https://icons8.com/" height="48px"/>'
-    docker_rpi_icon = '<img src="https://raw.githubusercontent.com/NeblioTeam/neblio/master/doc/img/docker-rpi-100.png" alt="Docker & Raspberry Pi Icon by icons8 from https://icons8.com/" height="48px"/>'
+    win_icon        = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/windows-10-100.png" alt="Windows Icon by icons8 from https://icons8.com/" height="48px"/>'
+    mac_icon        = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/apple-100.png" alt="Apple Icon by icons8 from https://icons8.com/" height="48px"/>'
+    lin_icon        = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/linux-100.png" alt="Linux Icon by icons8 from https://icons8.com/" height="48px"/>'
+    rpi_icon        = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/raspberry-pi-100.png" alt="Raspberry Pi Icon by icons8 from https://icons8.com/" height="48px"/>'
+    docker_icon     = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/docker-100.png" alt="Docker Icon by icons8 from https://icons8.com/" height="48px"/>'
+    docker_rpi_icon = '<img src="https://raw.githubusercontent.com/macpuffin/macpuffins/master/doc/img/docker-rpi-100.png" alt="Docker & Raspberry Pi Icon by icons8 from https://icons8.com/" height="48px"/>'
 
     rel = repo.release_from_tag(os.environ['GITHUB_REF'].rsplit('/', 1)[1])
 
@@ -37,50 +37,50 @@ def check_assets():
 
     for x in assets:
         global win_q_str
-        if ("neblio-Qt---windows_x86" in x.name and len(win_q_str) == 0):
+        if ("macpuffins-Qt---windows_x86" in x.name and len(win_q_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 win_q_str = '| ' + win_icon + ' | [Download ' + rel.tag_name + '<br/>For Windows](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(win_q_str)
 
         global mac_q_str
-        if ("neblio-Qt---macOS" in x.name and len(mac_q_str) == 0):
+        if ("macpuffins-Qt---macOS" in x.name and len(mac_q_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 mac_q_str = '| ' + mac_icon + ' | [Download ' + rel.tag_name + '<br/>For macOS](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(mac_q_str)
 
         global lin_q_str
-        if ("neblio-Qt---ubuntu16.04" in x.name and len(lin_q_str) == 0):
+        if ("macpuffins-Qt---ubuntu16.04" in x.name and len(lin_q_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 lin_q_str = '| ' + lin_icon + ' | [Download ' + rel.tag_name + '<br/>For Linux](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(lin_q_str)
 
         global rpi_q_str
-        if ("neblio-Qt---RPi-raspbian" in x.name and len(rpi_q_str) == 0):
+        if ("macpuffins-Qt---RPi-raspbian" in x.name and len(rpi_q_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 rpi_q_str = '| ' + rpi_icon + ' | [Download ' + rel.tag_name + '<br/>For Raspberry Pi](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(rpi_q_str)
 
         global lin_d_str
-        if ("nebliod---ubuntu16.04" in x.name and len(lin_d_str) == 0):
+        if ("macpuffinsd---ubuntu16.04" in x.name and len(lin_d_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 lin_d_str = '| ' + lin_icon + ' | [Download ' + rel.tag_name + '<br/>For Linux](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(lin_d_str)
 
         global rpi_d_str
-        if ("nebliod---RPi-raspbian" in x.name and len(rpi_d_str) == 0):
+        if ("macpuffinsd---RPi-raspbian" in x.name and len(rpi_d_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 rpi_d_str = '| ' + rpi_icon + ' | [Download ' + rel.tag_name + '<br/>For Raspberry Pi](' + x.browser_download_url + ') | `' + x.sha256 + '` |'
                 # print(rpi_d_str)
 
-        # post docker nebliod build once regular nebliod build is done building, since we cannot easily tell when the docker builds are done
+        # post docker macpuffinsd build once regular macpuffinsd build is done building, since we cannot easily tell when the docker builds are done
         global docker_d_str
-        if ("nebliod---ubuntu16.04" in x.name and len(docker_d_str) == 0):
+        if ("macpuffinsd---ubuntu16.04" in x.name and len(docker_d_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 docker_d_str = '| ' + docker_icon + ' | [Download ' + rel.tag_name + '<br/>For Docker](' + 'https://hub.docker.com/r/neblioteam/nebliod/builds' + ') | `' + 'N/A' + '` |'
@@ -88,14 +88,14 @@ def check_assets():
 
         # post docker RPi build once regular RPi Qt build is done building, since we cannot easily tell when the docker builds are done
         global docker_rpi_d_str
-        if ("neblio-Qt---RPi-raspbian" in x.name and len(docker_rpi_d_str) == 0):
+        if ("macpuffins-Qt---RPi-raspbian" in x.name and len(docker_rpi_d_str) == 0):
             x.sha256 = download_and_checksum(x.browser_download_url)
             if len(x.sha256) == 64:
                 docker_rpi_d_str = '| ' + docker_rpi_icon + ' | [Download ' + rel.tag_name + '<br/>For Docker on RPi (ARMv6hf)](' + 'https://hub.docker.com/r/neblioteam/nebliod-rpi/builds' + ') | `' + 'N/A' + '` |'
                 # print(rpi_d_str)
 
     # build our release body table
-    body.append('## neblio-Qt (Wallet with User Interface)')
+    body.append('## macpuffins-Qt (Wallet with User Interface)')
     body.append('| System | Download | Sha256 Checksum |')
     body.append('|:---:|:---:|:---|')
     if len(win_q_str) > 0:
@@ -116,7 +116,7 @@ def check_assets():
     	body.append('| ' + rpi_icon + ' | Build In Progress.<br/>Refresh This Page In A Moment. | - |')
     body.append('')
     body.append('')
-    body.append('## nebliod (Server Node. Command Line Only)')
+    body.append('## macpuffinsd (Server Node. Command Line Only)')
     body.append('| System | Download | Sha256 Checksum |')
     body.append('|:---:|:---:|:---|')
     if len(lin_d_str) > 0:
@@ -157,10 +157,10 @@ def check_assets():
 def update_release_text(existing_body, new_body):
     existing_body_lines = existing_body.split('\r\n')
     # check if our release table already exists
-    if ('## neblio-Qt (Wallet with User Interface)' in existing_body_lines and
-        '## nebliod (Server Node. Command Line Only)' in existing_body_lines):
-        # slice out everything after ## neblio-Qt (Wallet with User Interface)
-        index = existing_body_lines.index('## neblio-Qt (Wallet with User Interface)')
+    if ('## macpuffins-Qt (Wallet with User Interface)' in existing_body_lines and
+        '## macpuffinsd (Server Node. Command Line Only)' in existing_body_lines):
+        # slice out everything after ## macpuffins-Qt (Wallet with User Interface)
+        index = existing_body_lines.index('## macpuffins-Qt (Wallet with User Interface)')
         original_body = existing_body_lines[:index]
         original_body.append(new_body)
         return "\r\n".join(original_body)
