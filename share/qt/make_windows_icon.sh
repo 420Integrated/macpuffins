e66e47db369442719afb705ec7510aac2c9dc9e4
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-01-22 14:15:55 -0400

    macpuffins

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/neblio.ico
+ICON_DST=../../src/qt/res/icons/macpuffins.ico
 
-convert ../../src/qt/res/icons/neblio-16.png ../../src/qt/res/icons/neblio-32.png ../../src/qt/res/icons/neblio-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/macpuffins-16.png ../../src/qt/res/icons/macpuffins-32.png ../../src/qt/res/icons/macpuffins-48.png ${ICON_DST}
