b6b39df02fb3deaeded9ddea5ce9714a27ee74fb
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-01-22 16:57:26 -0400

    macpuffins

--- wallet/miner.cpp
+++ wallet/miner.cpp
@@ -584,7 +584,7 @@ void StakeMiner(CWallet* pwallet)
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
 
     // Make this thread recognisable as the mining thread
-    RenameThread("neblio-miner");
+    RenameThread("macpuffins-miner");
 
     bool fTryToSync = true;
 
