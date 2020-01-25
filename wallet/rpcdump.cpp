7447bd92e8b67003d1bf5c060b5a64e0c413c96d
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-01-25 15:01:43 -0400

    macpuffins

--- wallet/rpcdump.cpp
+++ wallet/rpcdump.cpp
@@ -114,7 +114,7 @@ public:
 Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
-        throw runtime_error("importprivkey <neblioprivkey> [label]\n"
+        throw runtime_error("importprivkey <macpuffinsprivkey> [label]\n"
                             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -250,15 +250,15 @@ Value importwallet(const Array& params, bool fHelp)
 Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
-        throw runtime_error("dumpprivkey <neblioaddress>\n"
-                            "Reveals the private key corresponding to <neblioaddress>.");
+        throw runtime_error("dumpprivkey <macpuffinsaddress>\n"
+                            "Reveals the private key corresponding to <macpuffinsaddress>.");
 
     EnsureWalletIsUnlocked();
 
     string          strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid neblio address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid macpuffins address");
     if (fWalletUnlockStakingOnly)
         throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Wallet is unlocked for staking only.");
     CKeyID keyID;
@@ -302,7 +302,7 @@ Value dumpwallet(const Array& params, bool fHelp)
     std::sort(vKeyBirth.begin(), vKeyBirth.end());
 
     // produce output
-    file << strprintf("# Wallet dump created by neblio %s (%s)\n", CLIENT_BUILD.c_str(),
+    file << strprintf("# Wallet dump created by macpuffins %s (%s)\n", CLIENT_BUILD.c_str(),
                       CLIENT_DATE.c_str());
     file << strprintf("# * Created on %s\n", EncodeDumpTime(GetTime()).c_str());
     file << strprintf("# * Best block at time of backup was %i (%s),\n", nBestHeight.load(),
